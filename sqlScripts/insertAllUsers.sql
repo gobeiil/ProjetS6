ccb83eb9808ca1cfe2b0bcea061cdb5bee343040
Benjamin Gobeil <Benjamin Gobeil>
2016-05-31 15:18:31 -0400

    Commit insertAllUsersScript.sql

---
+++ sqlScripts/insertAllUsers.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (administrative_user_id, last_name, first_name, email_address) VALUES ('gobb2201', 'Gobeil', 'Benjamin', 'Benjamin.Gobeil@USherbrooke.ca');
+INSERT INTO users (administrative_user_id, last_name, first_name, email_address) VALUES ('aubm2009', 'Aubé', 'Marc-Antoine', 'Marc-Antoine.Aube@USherbrooke.ca');
+INSERT INTO users (administrative_user_id, last_name, first_name, email_address) VALUES ('cotg1906', 'Côté', 'Gabriel', 'Gabriel.Cote@USherbrooke.ca');
+INSERT INTO users (administrative_user_id, last_name, first_name, email_address) VALUES ('fria1501', 'Frigon', 'Alexandre', 'Alexandre.Frgon@USherbrooke.ca');
+INSERT INTO users (administrative_user_id, last_name, first_name, email_address) VALUES ('elsf2301', 'El Sayed', 'Firas', 'Firas.El.Sayed@USherbrooke.ca');
+INSERT INTO users (administrative_user_id, last_name, first_name, email_address) VALUES ('tabj2001', 'Jérémy', 'Tablet', 'Jeremy.Tablet@USherbrooke.ca');
